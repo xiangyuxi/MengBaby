a852a49ab82550663b4c981e09606d09ffd23e1a
pgyx-yxiang <yxiang@YX.local>
2017-08-15 17:24:47 +0800

    first commit

---
+++ MengBaby/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MengBaby
+//
+//  Created by yxiang on 2017/8/15.
+//  Copyright © 2017年 yxiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MengBaby/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MengBaby
+//
+//  Created by yxiang on 2017/8/15.
+//  Copyright © 2017年 yxiang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
