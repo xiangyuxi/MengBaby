bbe30f3562760aa5395ab56e0f0a8d223c1dbd39
pgyx-yxiang <yxiang@YX.local>
2017-08-15 17:59:19 +0800

    Basic fromwork support

---
+++ MengBaby/MengBaby-Const.h
@@ -0,0 +1,16 @@
+//
+//  MengBaby-Const.h
+//  MengBaby
+//
+//  Created by yxiang on 2017/8/15.
+//  Copyright © 2017年 yxiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#define COLOR_OF_HEX(_H) [UIColor colorWithHex:_H]
+#define COLOR_OF_HEXALPHA(_H,_A) [UIColor colorWithHex:_H alpha:_A]
+
+UIKIT_EXTERN const NSString* LCAppID;;
+UIKIT_EXTERN const NSString* LCAppKey;
+UIKIT_EXTERN const NSString* LCMasterKey;
---
+++ MengBaby/MengBaby-Const.m
@@ -0,0 +1,13 @@
+//
+//  MengBaby-Const.m
+//  MengBaby
+//
+//  Created by yxiang on 2017/8/15.
+//  Copyright © 2017年 yxiang. All rights reserved.
+//
+
+#import "MengBaby-Const.h"
+
+const NSString* LCAppID = @"IdFUc9UTTkIdRFi9156TJFNA-gzGzoHsz";
+const NSString* LCAppKey = @"zNU1lo0Ip6CEnys1uan7rDoF";
+const NSString* LCMasterKey = @"AaI3goQg3qee9oCdq4PvnGXF";
