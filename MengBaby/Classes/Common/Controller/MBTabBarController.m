96eb6d56b639448c51daa2c25ed0ed3aa4d74691
pgyx-yxiang <yxiang@YX.local>
2017-08-16 18:11:40 +0800

    Meng Circle

--- MengBaby/Classes/Common/Controller/MBTabBarController.m
+++ MengBaby/Classes/Common/Controller/MBTabBarController.m
@@ -8,6 +8,15 @@
 
 #import "MBTabBarController.h"
 
+static UITabBarItem* createItem(NSString* nroImage, NSString* title) {
+    UIImage * nor = [[UIImage imageNamed:nroImage] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    UIImage * sel = [[UIImage imageNamed:[NSString stringWithFormat:@"%@_sel",nroImage]] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    UITabBarItem * item = [[UITabBarItem alloc] initWithTitle:title image:nor selectedImage:sel];
+    [item setTitleTextAttributes:@{NSForegroundColorAttributeName:COLOR_OF_HEX(0x8f8f8f)} forState:UIControlStateNormal];
+    [item setTitleTextAttributes:@{NSForegroundColorAttributeName:MENG_BLACK_COLOR} forState:UIControlStateSelected];
+    return item;
+}
+
 @interface MBTabBarController ()
 
 @end
@@ -17,9 +26,11 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.tabBar.tintColor = MENG_BLACK_COLOR;
+    NSArray* imageNames = @[@"meng_circle_tabbar_icon",@""];
+    NSArray* titleNames = @[@"萌圈",@"Item"];
     for (UIViewController* viewController in self.viewControllers) {
-        viewController.tabBarItem.image = [viewController.tabBarItem.image imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+        NSInteger index = [self.viewControllers indexOfObject:viewController];
+        viewController.tabBarItem = createItem(imageNames[index], titleNames[index]);
     }
 }
 
